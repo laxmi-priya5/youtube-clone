f0d0795440e6433022494be8a4c5b29ace33907f
Laxmipriya <laxmi.priyar2005@gmail.com>
2026-01-04 15:39:48 +0530

    add changes

--- src/Components/Catagory.jsx
+++ src/Components/Catagory.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import Button from './Button'
 const Catagory = () => {
   return (
-    <div className='mb-5'>
+    <div className='mb-5 flex gap-4 overflow-hidden'>
         
         <Button name='All'/>
+        <Button name='Music'/>
+        <Button name='Indian Idol'/>
+        <Button name='Podcast'/>
+        <Button name='Mixes'/>
+        <Button name='Live'/>
+        <Button name='Self-confidence'/>
+        <Button name='Intelligence'/>
+        <Button name='Practice'/>
+        
     </div>
   )
 }
--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Catagory from './Catagory'
 import VideoContainer from './videoContainer'
-
+import { useSelector } from 'react-redux'
 const MainContainer = () => {
+  const toggle = useSelector(store=>store.app.isMenuOpen)
+  console.log(toggle)
   return (
-    <div className='p-5 ml-50 mt-20'>
+    <div className='p-5  mt-20' style={{ marginLeft: toggle ? "200px" : "0px" }}>
       <Catagory/>
       <VideoContainer/>
     </div>
